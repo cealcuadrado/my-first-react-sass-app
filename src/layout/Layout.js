2296a6dc6cc145066860967ac56b715d76504b08
CeAlCuadrado <cealcuadrado@outlook.com>
2021-10-14 09:02:56 -0300

    Instaladas dependencias auxiliares, creado componente Layout

---
+++ src/layout/Layout.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Layout.scss';
+
+class Layout extends React.Component {
+    render() {
+        return (
+            <div className="layout">
+                <p>Hola, mundo!</p>
+            </div>
+        );
+    }
+}
+
+export default Layout;
